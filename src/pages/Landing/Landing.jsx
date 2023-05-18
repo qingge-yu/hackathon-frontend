80249e240241c3b1498f7b5661f2a71ede38556c
Brian4755 <brian.mccune1@yahoo.com>
2023-05-18 15:52:45 -0500

    landing page change

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,7 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hi, {user ? user.name : 'friend'}</h1>
+      <h1>hi {user ? user.name : 'friend'}</h1>
     </main>
   )
 }
