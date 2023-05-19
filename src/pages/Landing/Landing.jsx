8d7fff890f200e92b2bd30b896abc123eec3bf33
Chris McClarin <eternalrook@gmail.com>
2023-05-19 14:46:06 -0700

    modalcomplete

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,33 @@
 import styles from './Landing.module.css'
+import Popup from '../../components/Pop-up/modal'
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Landing = ({ user }) => {
+
+  const [modalShow, setModalShow] = useState(false);
+
+
   return (
-    <main className={styles.container}>
-      <h1>hi {user ? user.name : 'friend'}</h1>
-    </main>
+    <body>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+        crossorigin="anonymous"
+      />
+      <main className={styles.container}>
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+          Launch vertically centered modal
+        </Button>
+        <Popup
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+        {/* <h1>hi {user ? user.name : 'friend'}</h1> */}
+      </main>
+    </body>
   )
 }
 
