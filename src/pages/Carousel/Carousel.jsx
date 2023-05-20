a30bde10db6500a26eabef30cf7d3a992c1b92bf
Brian4755 <brian.mccune1@yahoo.com>
2023-05-19 19:38:55 -0500

    base button setup for lesson carousel

--- src/pages/Carousel/Carousel.jsx
+++ src/pages/Carousel/Carousel.jsx
@@ -1,21 +1,32 @@
-import { useState } from "react";
+import './Carousel.css'
+import { useState} from "react";
 
 const Carousel = () => {
-  const [lessonPage, setLessonPage] = useState()
+  const [lessonPage, setLessonPage] = useState(1)
 
-  function handleCount() {
-    
+  function handleCountMinus() {
+    if (lessonPage > 1) {
+      setLessonPage(lessonPage - 1)
+    } else {
+      setLessonPage(9)
+    }
   }
+
+  function handleCountPlus() {
+    if (lessonPage < 9) {
+      setLessonPage(lessonPage + 1)
+    } else {
+      setLessonPage(1)
+    }
+  }
+
+
   return ( 
-    <>
-      <div>
-        <button>Lesson 8</button>
-        <button>Lesson 9</button>
-        <button onClick={handleCount}>Lesson 1</button>
-        <button>Lesson 2</button>
-        <button>Lesson 3</button>
-      </div>
-    </>
+    <div className='lessonCarouselBtn'>
+        <button onClick={handleCountMinus}> Lesson {lessonPage === 1 ? 9 :(lessonPage - 1)} </button>
+        <p> Lesson {lessonPage}</p>
+        <button onClick={handleCountPlus}> Lesson {lessonPage === 9 ? 1 : lessonPage + 1} </button>
+    </div>
    );
 }
  
