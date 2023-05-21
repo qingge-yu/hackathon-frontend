24956bc3fe0fb9b7965802fefa037c1dde5b69ff
qingge-yu <maggieqgg@gmail.com>
2023-05-21 16:26:41 -0700

    finishes glossary list layout, adds background image on each item, sets up bold effect for selected button

--- src/pages/GlossaryPage/Glossary.jsx
+++ src/pages/GlossaryPage/Glossary.jsx
@@ -1,5 +1,6 @@
 //Import modules
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import GlossaryDefinition from '../../components/GlossaryComponent/GlossaryDefinition'
 import Button from 'react-bootstrap/Button';
 
@@ -11,36 +12,78 @@ const Glossary = () => {
 
     // To make definitions pop up when clicked
     const [GDefShow, setGDefShow] = useState("");
+    const [selectedButton, setSelectedButton] = useState(null)
 
     function handleLessonClick(name) {
         setGDefShow(name)
     }
 
+    const handleButtonClick = (buttonText) => {
+        setSelectedButton(buttonText);
+    };
+
     return (
         <div>
             <div className="glossary-container">
-                <ul className="glossary-list">
+                <p className="directoryPath">
+                    <Link to="/" className="homeLink">Home</Link> / Glossary
+                </p>
+                <div className="glossary-left">
                     <h1>Glossary</h1>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Browser')}>Browser</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('URL')}>URL</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Hyperlink')}>Hyperlink</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Search Engine')}>Search Engine</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Software')}>Software</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Program')}>Program</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Website')}>Website</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Tabs')}>Tabs</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Bookmarks')}>Bookmarks</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Back Button')}>Back Button</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Forward Button')}>Forward Button</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('Refresh')}>Refresh</Button></li>
-                    <li><Button variant="link" onClick={() => handleLessonClick('History')}>History</Button></li>
-                </ul>
+                    <ul className="glossary-list">
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Browser' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Browser'); handleButtonClick() }}
+                        >Browser</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'URL' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('URL'); handleButtonClick() }}
+                        >URL</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Search Engine' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Search Engine'); handleButtonClick() }}
+                        >Search Engine</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Software' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Software'); handleButtonClick() }}
+                        >Software</Button></li>
+                        <li className="gItem" ><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Program' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Program'); handleButtonClick() }}
+                        >Program</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Website' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Website'); handleButtonClick() }}
+                        >Website</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Tabs' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Tabs'); handleButtonClick() }}
+                        >Tabs</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Bookmarks' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Bookmarks'); handleButtonClick() }}
+                        >Bookmarks</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Back Button' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Back Button'); handleButtonClick() }}
+                        >Back Button</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Forward Button' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Forward Button'); handleButtonClick() }}
+                        >Forward Button</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'Refresh' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('Refresh'); handleButtonClick() }}
+                        >Refresh</Button></li>
+                        <li className="gItem"><Button variant="link"
+                            style={{ fontWeight: selectedButton === 'History' ? 'bold' : 'normal' }}
+                            onClick={() => { handleLessonClick('History'); handleButtonClick() }}
+                        >History</Button></li>
+                    </ul>
+                </div>
                 <GlossaryDefinition definitions={GDefShow} />
-                <svg className="glossary-definition" width="693" height="600" viewBox="0 0 693 816" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M693 443.78C693 670.587 549.079 816 357.712 816C166.345 816 0 670.587 0 443.78C0 216.973 187.701 0 379.068 0C570.434 0 693 216.973 693 443.78Z" fill="#F2CF66" />
-                </svg>
             </div>
-        </div>
+        </div >
     )
 }
 
