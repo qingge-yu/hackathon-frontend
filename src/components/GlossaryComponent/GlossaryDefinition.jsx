21fb9039c25c77b8a26873df5cdc4e99eb4d2650
qingge-yu <maggieqgg@gmail.com>
2023-05-21 10:23:48 -0700

    adds content to glossary page and glossary definition component

--- src/components/GlossaryComponent/GlossaryDefinition.jsx
+++ src/components/GlossaryComponent/GlossaryDefinition.jsx
@@ -1,11 +1,20 @@
 import "./GlossaryDefinition.css"
 
 const GlossaryDefinition = (props) => {
-    let Gdefinitions={
-        Browser: "A software Program that lets you view web pages on your computer.",
-        URL: "Universal ",
-        Hyperlink: "",
-        "Search Engine": "Hello"
+    let Gdefinitions = {
+        Browser: "A software Program that lets you view digital content on the internet.",
+        URL: 'The address that identifies a webpage on the internet. It usually starts with "http://" or "https://" followed by the domain name and specific page.',
+        Hyperlink: "A clickable link on a webpage that redirects users to another webpage or resource when clicked.",
+        "Search Engine": "A clickable link on a webpage that redirects users to another webpage or resource when clicked.",
+        Software: "The programs, applications, and other operating information used by a computer.",
+        Program: "A series of coded software instructions to control the operation of a computer or other machine. Sometimes also referred to as an application.",
+        Website: "A collection of webpages and related content accessible through a specific domain or URL.",
+        Tabs: "A second or further document or page that can be opened on your web browser.",
+        Bookmarks: 'A method of saving the address of a web page. Sometimes they are referred to as “favorites".',
+        "Back Button": "A common web browser feature that navigates to the previous web page you were on.",
+        "Forward Button": "",
+        Refresh: "",
+        History: "",
     }
 
     return (
--- src/pages/GlossaryPage/Glossary.jsx
+++ src/pages/GlossaryPage/Glossary.jsx
@@ -8,7 +8,7 @@ import './Glossary.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Glossary = () => {
-    
+
     // To make definitions pop up when clicked
     const [GDefShow, setGDefShow] = useState("");
 
@@ -25,10 +25,19 @@ const Glossary = () => {
                     <li><Button variant="link" onClick={() => handleLessonClick('URL')}>URL</Button></li>
                     <li><Button variant="link" onClick={() => handleLessonClick('Hyperlink')}>Hyperlink</Button></li>
                     <li><Button variant="link" onClick={() => handleLessonClick('Search Engine')}>Search Engine</Button></li>
+                    <li><Button variant="link" onClick={() => handleLessonClick('Software')}>Software</Button></li>
+                    <li><Button variant="link" onClick={() => handleLessonClick('Program')}>Program</Button></li>
+                    <li><Button variant="link" onClick={() => handleLessonClick('Website')}>Website</Button></li>
+                    <li><Button variant="link" onClick={() => handleLessonClick('Tabs')}>Tabs</Button></li>
+                    <li><Button variant="link" onClick={() => handleLessonClick('Bookmarks')}>Bookmarks</Button></li>
+                    <li><Button variant="link" onClick={() => handleLessonClick('Back Button')}>Back Button</Button></li>
+                    <li><Button variant="link" onClick={() => handleLessonClick('Forward Button')}>Forward Button</Button></li>
+                    <li><Button variant="link" onClick={() => handleLessonClick('Refresh')}>Refresh</Button></li>
+                    <li><Button variant="link" onClick={() => handleLessonClick('History')}>History</Button></li>
                 </ul>
                 <GlossaryDefinition definitions={GDefShow} />
                 <svg className="glossary-definition" width="693" height="600" viewBox="0 0 693 816" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M693 443.78C693 670.587 549.079 816 357.712 816C166.345 816 0 670.587 0 443.78C0 216.973 187.701 0 379.068 0C570.434 0 693 216.973 693 443.78Z" fill="#F2CF66"/>
+                    <path d="M693 443.78C693 670.587 549.079 816 357.712 816C166.345 816 0 670.587 0 443.78C0 216.973 187.701 0 379.068 0C570.434 0 693 216.973 693 443.78Z" fill="#F2CF66" />
                 </svg>
             </div>
         </div>
